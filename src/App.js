2f8fbaa9a10b3d41eacbaae34c9be3540255bc1d
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-13 14:18:46 -0300

    Basic Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Home from './components/Home'
 import './theme/globals.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Readable</h1>
+        <h5>Wednesday, July 11, 2018</h5>
+      </div>
+    )
+  }
+}
+
+export default Home
