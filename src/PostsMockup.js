08fbdf14bb1dbb45657a40dd755e422b57c150ea
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-15 13:51:02 -0300

    Post mockup

---
+++ src/PostsMockup.js
@@ -0,0 +1,29 @@
+
+const posts = [
+  {
+    title: 'Resgatados de caverna na Tailândia perderam média de 2 kg, mas não têm problemas graves nem estresse',
+    category: 'World',
+    voteScore: 33,
+    body: 'Os 12 meninos e o seu técnico que foram resgatados da caverna Tham Luang, no norte da Tailândia, não têm problemas graves de saúde nem mostram sinais de estresse, mesmo ficando presos mais de duas semanas no local, informaram os médicos nesta quarta-feira (11). Eles perderam uma média de dois quilos e alguns, incluindo o treinador, têm quadros leves de infecção pulmonar.'
+  },
+  {
+    title: 'O que acontece com a taça da Copa do Mundo depois da comemoração',
+    category: 'World',
+    voteScore: 12,
+    body: 'Em um galpão simples, localizado em uma tranquila rua de uma região industrial de Milão, na Itália, sete ourives já estão prontos para receber a famosa taça, que vai ganhar o nome da seleção campeã da Copa da Rússia 2018.'
+  },
+  {
+    title: 'Resgatados de caverna na Tailândia perderam média de 2 kg, mas não têm problemas graves nem estresse',
+    category: 'World',
+    voteScore: 33,
+    body: 'Os 12 meninos e o seu técnico que foram resgatados da caverna Tham Luang, no norte da Tailândia, não têm problemas graves de saúde nem mostram sinais de estresse, mesmo ficando presos mais de duas semanas no local, informaram os médicos nesta quarta-feira (11). Eles perderam uma média de dois quilos e alguns, incluindo o treinador, têm quadros leves de infecção pulmonar.'
+  },
+  {
+    title: 'O que acontece com a taça da Copa do Mundo depois da comemoração',
+    category: 'World',
+    voteScore: 12,
+    body: 'Em um galpão simples, localizado em uma tranquila rua de uma região industrial de Milão, na Itália, sete ourives já estão prontos para receber a famosa taça, que vai ganhar o nome da seleção campeã da Copa da Rússia 2018.'
+  }
+]
+
+export default posts
--- src/components/ListPosts/index.js
+++ src/components/ListPosts/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import PostsMockup from '../../PostsMockup.js'
 
 class ListPosts extends Component {
   render() {
     return (
       <div>
         <ul>
-          <li>
-            <h2>Resgatados de caverna na Tailândia perderam média de 2 kg, mas não têm problemas graves nem estresse</h2>
-            <h5>World</h5>
-            <span>33</span>
-            <p>Os 12 meninos e o seu técnico que foram resgatados da caverna Tham Luang, no norte da Tailândia, não têm problemas graves de saúde nem mostram sinais de estresse, mesmo ficando presos mais de duas semanas no local, informaram os médicos nesta quarta-feira (11). Eles perderam uma média de dois quilos e alguns, incluindo o treinador, têm quadros leves de infecção pulmonar.</p>
-          </li>
+          {PostsMockup.map((post, key) => (
+            <li key={key}>
+              <h2>{post.title}</h2>
+              <h5>{post.category}</h5>
+              <span>{post.voteScore}</span>
+              <p>{post.body}</p>
+            </li>
+          ))}
         </ul>
       </div>
     )
