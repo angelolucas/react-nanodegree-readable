df4ca2ec26641be87641f5162314834c361be4b1
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-16 13:36:16 -0300

    Inject globals styles with aphrodite-globals module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import injectGlobalStyles from 'aphrodite-globals'
+import globals from './theme/globals'
 import Home from './components/Home'
-import './theme/globals.css'
+
+injectGlobalStyles(globals)
 
 class App extends Component {
   render() {
---
+++ src/theme/globals.js
@@ -0,0 +1,27 @@
+const globals = {
+  '*': {
+    boxSizing: 'borber-box',
+  },
+
+  body: {
+    fontFamily: '"Merriweather", serif',
+    color: '#222',
+    backgroundColor: '#F0F1EB',
+    margin: 0,
+    overflowX: 'hidden',
+    fontSmoothing: 'antialiased',
+    textRendering: 'optimizeLegibility',
+    '-moz-osx-font-smoothing': 'grayscale',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'h1, h2, h3, h4, h5, h6': { margin: 0 },
+
+  ul: {
+    margin: 0,
+    padding: 0,
+    listStyle: 'none',
+  },
+}
+
+export default globals
