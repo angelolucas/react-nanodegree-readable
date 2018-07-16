57a9632500da62fdae7aa91e4ba3f251b0fccddc
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-16 10:51:40 -0300

    List posts style

--- src/components/ListPosts/index.js
+++ src/components/ListPosts/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import PostsMockup from '../../PostsMockup.js'
+import { StyleSheet, css } from 'aphrodite/no-important'
 
 class ListPosts extends Component {
   render() {
     return (
       <div>
-        <ul>
+        <ul className={css(styles.list)}>
           {PostsMockup.map((post, key) => (
-            <li key={key}>
+            <li className={css(styles.post)} key={key}>
               <h2>{post.title}</h2>
               <h5>{post.category}</h5>
               <span>{post.voteScore}</span>
@@ -19,5 +20,15 @@ class ListPosts extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  list: {
+    columnCount: 3,
+  },
+  post: {
+    padding: 20,
+    display: 'inline-block',
+  }
+})
 
 export default ListPosts
