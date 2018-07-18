2088928ef4c2a39d37cc7cfe5a81c1606d8f37b3
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-18 10:22:04 -0300

    First connection with API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import injectGlobalStyles from 'aphrodite-globals'
 import globals from './theme/globals'
 import Home from './components/Home'
+import * as API from './API'
 
 injectGlobalStyles(globals)
 
 class App extends Component {
   render() {
+    API.getPosts().then((post) => {
+      console.log(post)
+    })
     return (
       <div>
         <Home />
