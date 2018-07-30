2709ea6fa4686d445f9312581c00d2c205ea36d5
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-30 13:58:33 -0300

    store in a separated file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react'
 import injectGlobalStyles from 'aphrodite-globals'
 import globals from './theme/globals'
 import Home from './components/Home'
-import * as API from './API'
 
 injectGlobalStyles(globals)
 
 class App extends Component {
   render() {
-    API.getPosts().then((post) => {
-      console.log(post)
-    })
     return (
       <div>
         <Home />
