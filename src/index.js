0fec11c7495b988958a18c0e917b44c24b4bb075
Ângelo Lucas <angelo.lrc@gmail.com>
2018-07-31 14:49:04 -0300

    Delete some initial files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('[data-app]')
 )
-
-registerServiceWorker()
