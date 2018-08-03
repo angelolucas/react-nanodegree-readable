555357b6a8e761704598a70fe5fbcd3b46decb60
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-03 15:03:04 -0300

    Breakpoint

---
+++ src/theme/breakpoint.js
@@ -0,0 +1,6 @@
+const breakpoint = {
+  medium: '@media (max-width: 1200px)',
+  small: '@media (max-width: 800px)',
+}
+
+export default breakpoint
--- src/theme/index.js
+++ src/theme/index.js
@@ -2,5 +2,6 @@ import colors from './colors'
 import fonts from './fonts'
 import globals from './globals'
 import spaces from './spaces'
+import breakpoint from './breakpoint'
 
-export { colors, fonts, globals, spaces }
+export { colors, fonts, globals, spaces, breakpoint }
