b6d22d7475d04715c6d0e8c46283144e66d99787
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-09 17:04:59 -0300

    Slugify refactoring

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,20 +1,7 @@
-/**
- * slugify('Learn Redux in 10 minutes!') = learn-redux-in-10-minutes
- * https://gist.github.com/mathewbyrne/1280286
- */
+import slugify from 'slugify'
 
-export default text => {
-  const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;'
-  const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-
-  return text
-    .toString()
-    .toLowerCase()
-    .trim()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special chars
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w-]+/g, '') // Remove all non-word chars
-    .replace(/--+/g, '-') // Replace multiple - with single -
-}
+export default text =>
+  slugify(text, {
+    remove: /[*+~.()'"!:@—]/g, // regex to remove characters
+    lower: true, // result in lower case
+  })
