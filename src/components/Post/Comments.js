71ef33f837891b4b2cadcab741b1480f8874dece
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-10 11:00:11 -0300

    Some CSS on list comments

--- src/components/Post/Comments.js
+++ src/components/Post/Comments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { StyleSheet, css } from 'aphrodite/no-important'
 import { spaces } from '../../theme'
+import date from '../../utils/date'
 
 class Comments extends Component {
   render() {
@@ -18,12 +19,16 @@ class Comments extends Component {
         <ul>
           {comments &&
             comments.map(comment => (
-              <li key={comment.id}>
-                <h6>{comment.author}</h6>
-                <p>{comment.body}</p>
-                <span>{comment.voteScore}</span>
-                <span>Edit</span>
-                <span>Delete</span>
+              <li className={css(styles.comment)} key={comment.id}>
+                <p className={css(styles.body)}>{comment.body}</p>
+                <span className={css(styles.author)}>{comment.author}</span>
+                <span className={css(styles.date)}>
+                  {date(comment.timestamp)}
+                </span>
+                <span className={css(styles.voteScore)}>
+                  {comment.voteScore}
+                </span>
+                <span className={css(styles.edit)}>edit</span>
               </li>
             ))}
         </ul>
@@ -34,6 +39,29 @@ class Comments extends Component {
 
 Comments.propTypes = { comments: PropTypes.array }
 
-const styles = StyleSheet.create({ comments: { marginBottom: spaces.x3 } })
+const styles = StyleSheet.create({
+  comments: { marginBottom: spaces.x3 },
+  comment: {
+    borderBottom: '1px solid',
+    marginBottom: spaces.x2,
+    paddingBottom: spaces.x2,
+
+    ':last-child': {
+      borderBottom: 'none',
+      paddingBottom: 0,
+    },
+  },
+  author: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginRight: spaces.x2,
+  },
+  date: { marginRight: spaces.x2 },
+  edit: {
+    float: 'right',
+    fontSize: 11,
+    padding: 10,
+  },
+})
 
 export default Comments
