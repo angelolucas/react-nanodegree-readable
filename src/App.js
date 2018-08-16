7d5848bd5ad2edf1dc4d4e716202ce7e2b8897c0
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-16 13:27:02 -0300

    Receive posts and categories before render app

--- src/App.js
+++ src/App.js
@@ -27,58 +27,54 @@ import Footer from './components/Footer'
 injectGlobalStyles(globals)
 
 class App extends Component {
-  state = { initialContent: false }
-
   UNSAFE_componentWillMount() {
     // Dispatch initial content
-    // TODO: check if both was received before trigger render
     this.props.dispatch(fetchPosts())
-    this.props.dispatch(fetchCategories()).then(() => {
-      this.setState({ initialContent: true })
-    })
+    this.props.dispatch(fetchCategories())
   }
   render() {
     const { categories, posts } = this.props
 
     return (
       <Router>
-        {this.state.initialContent && (
-          <div className={css(styles.general)}>
-            <Header />
-            <Switch>
-              {/* Home page */}
-              <Route exact path="/" component={Home} />
+        {categories &&
+          posts && (
+            <div className={css(styles.general)}>
+              <Header />
+              <Switch>
+                {/* Home page */}
+                <Route exact path="/" component={Home} />
 
-              {/* Category page */}
-              {categories.map(category => (
-                <Route
-                  exact
-                  path={`/${category.path}`}
-                  key={category.path}
-                  render={() => (
-                    <Category name={category.name} path={category.path} />
-                  )}
-                />
-              ))}
+                {/* Category page */}
+                {categories.map(category => (
+                  <Route
+                    exact
+                    path={`/${category.path}`}
+                    key={category.path}
+                    render={() => (
+                      <Category name={category.name} path={category.path} />
+                    )}
+                  />
+                ))}
 
-              {/* Post page */}
-              {posts.map(post => (
-                <Route
-                  path={`/${post.category}/${slugify(post.title)}`}
-                  key={post.id}
-                  render={() => <Post post={post} />}
-                />
-              ))}
+                {/* Post page */}
+                {posts.map(post => (
+                  <Route
+                    path={`/${post.category}/${slugify(post.title)}`}
+                    key={post.id}
+                    render={() => <Post post={post} />}
+                  />
+                ))}
 
-              {/* New post page*/}
-              <Route path="/new-post" component={CreatePost} />
+                {/* New post page*/}
+                <Route path="/new-post" component={CreatePost} />
 
-              {/* Redirect to home if Routes above don't match */}
-              <Redirect to="/" />
-            </Switch>
-            <Footer />
-          </div>
-        )}
+                {/* Redirect to home if Routes above don't match */}
+                <Redirect to="/" />
+              </Switch>
+              <Footer />
+            </div>
+          )}
       </Router>
     )
   }
