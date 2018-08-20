f18f4f73d004c9b20487e84a975594b65ef9e213
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-20 13:54:15 -0300

    Improvement in server logs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 require('dotenv').config()
-const chalk = require('chalk')
-const log = console.log
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -9,6 +7,7 @@ const config = require('./config')
 const categories = require('./categories')
 const posts = require('./posts')
 const comments = require('./comments')
+const log = require('./log')
 
 const app = express()
 
@@ -130,210 +129,244 @@ app.use((req, res, next) => {
 })
 
 app.get('/categories', (req, res) => {
-  log(chalk.cyan.bold('\nGET /:categories'))
+  log('get', '/categories')
 
   categories.getAll(req.token).then(
-    data => res.send(data),
+    data => {
+      log('success', `All (${data.length}) categories have been provided`)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.get('/:category/posts', (req, res) => {
-  log(chalk.cyan.bold('\nGET /:category/posts'))
-  log(chalk.gray('USAGE: Get all of the categories available for the app.'))
-  log('category: ', req.params.category)
+  log('get', `/${req.params.category}/posts`)
 
   posts.getByCategory(req.token, req.params.category).then(
-    data => res.send(data),
+    data => {
+      log(
+        'success',
+        `All (${data.length}) posts of the ${
+          req.params.category
+        } category have been provided`
+      )
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.get('/posts', (req, res) => {
-  log(chalk.cyan.bold('\nGET /posts'))
-  log(chalk.gray('USAGE Get all of the posts for a particular category'))
+  log('get', '/posts')
 
   posts.getAll(req.token).then(
-    data => res.send(data),
+    data => {
+      log('success', `All (${data.length}) posts have been provided`)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.post('/posts', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPOST /posts'))
-  log(chalk.gray('USAGE: Add a new post'))
-  log(req.body)
+  log('post', '/posts')
 
   posts.add(req.token, req.body).then(
-    data => res.send(data),
+    data => {
+      log('success', 'A new post has been added')
+      log('', data)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.get('/posts/:id', (req, res) => {
-  log(chalk.cyan.bold('\nGET /posts/:id'))
-  log(chalk.gray('USAGE: Get the details of a single post'))
-  log('id ', req.params.id)
+  log('get', `/posts/${req.params.id}`)
 
   posts.get(req.token, req.params.id).then(
-    data => res.send(data),
+    data => {
+      log('success', `Details of the post '${data.title}' have been provided`)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.post('/posts/:id', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPOST /posts/:id - (voting on a post)'))
-  log(req.body)
+  log('post', `/posts/${req.params.id}`)
 
   const { option } = req.body
   const id = req.params.id
 
   posts.vote(req.token, id, option).then(
-    data => res.send(data),
+    data => {
+      log('success', `Post has been '${option}'`)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.put('/posts/:id', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPUT /posts/:id'))
-  log(chalk.gray('USAGE: Edit the details of an existing post'))
-  log(req.body)
+  log('put', `/posts/${req.params.id}`)
 
   posts.edit(req.token, req.params.id, req.body).then(
-    data => res.send(data),
+    data => {
+      log('success', 'Post has been updated:')
+      log('', data)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.delete('/posts/:id', (req, res) => {
-  log(chalk.cyan.bold('\nDELETE /posts/:id'))
-  log('post id: ', req.params.id)
+  log('delete', `/posts/${req.params.id}`)
 
   posts
     .disable(req.token, req.params.id)
     .then(post => comments.disableByParent(req.token, post))
     .then(
-      data => res.send(data),
+      data => {
+        log('success', `Post has been deleted`)
+        return res.send(data)
+      },
       error => {
-        console.error(error)
+        log('error', error)
         res.status(500).send({ error: 'There was an error.' })
       }
     )
 })
 
 app.get('/posts/:id/comments', (req, res) => {
-  log(chalk.cyan.bold('\nGET /posts/:id/comments'))
-  log(chalk.gray('USAGE: Get all the comments for a single post'))
+  log('get', `/posts/${req.params.id}/comments`)
 
   comments.getByParent(req.token, req.params.id).then(
     data => {
-      log(data.length + ' comment(s)')
-
+      log(
+        'success',
+        `All (${data.length}) the comments of the post have been provided`
+      )
       return res.send(data)
     },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.post('/comments', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPOST /comments'))
-  log(chalk.gray('USAGE: Add a comment to a post'))
-  log(req.body)
+  log('post', '/comments')
 
   comments.add(req.token, req.body).then(
-    data => res.send(data),
+    data => {
+      log('success', 'The new comment has been added:')
+      log('', data)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.get('/comments/:id', (req, res) => {
-  log(chalk.cyan.bold('\nGET /comments/:id'))
-  log(chalk.gray('USAGE: Get the details for a single comment'))
-  log('comment id: ', req.params.id)
+  log('get', `/comments/${req.params.id}`)
 
   comments.get(req.token, req.params.id).then(
-    data => res.send(data),
+    data => {
+      log(
+        'success',
+        `Details of the author's comment ${data.author} were provided`
+      )
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.post('/comments/:id', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPOST /comments/:id'))
-  log(chalk.gray('USAGE: Used for voting on a comment.'))
+  log('post', `/comments/${req.params.id}`)
 
   const { option } = req.body
 
   comments.vote(req.token, req.params.id, option).then(
-    data => res.send(data),
+    data => {
+      log(
+        'success',
+        `The comment of the author ${data.author} has been '${option}'`
+      )
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.put('/comments/:id', bodyParser.json(), (req, res) => {
-  log(chalk.cyan.bold('\nPUT /comments/:id'))
-  log(chalk.gray('USAGE: Edit the details of an existing comment'))
-  log('post id: ', req.params.id)
-  log('changes: ', req.body)
+  log('put', `/comments/${req.params.id}`)
 
   comments.edit(req.token, req.params.id, req.body).then(
-    data => res.send(data),
+    data => {
+      log('success', 'The comment has been updated:')
+      log('', data)
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.delete('/comments/:id', (req, res) => {
-  log(chalk.cyan.bold('\nDELETE /comments/:id'))
-  log(chalk.gray("USAGE: Sets a comment's deleted flag to `true`"))
-  log('id: ', req.params.id)
+  log('delete', `/comments/${req.params.id}`)
 
   comments.disable(req.token, req.params.id).then(
-    data => res.send(data),
+    data => {
+      log(
+        'success',
+        `The comment of the author ${data.author} has been deleted`
+      )
+      return res.send(data)
+    },
     error => {
-      console.error(error)
+      log('error', error)
       res.status(500).send({ error: 'There was an error.' })
     }
   )
 })
 
 app.listen(config.port, () => {
-  log(
-    chalk.black.bold.bgCyan('Server listening on port %s, Ctrl+C to stop'),
-    config.port
-  )
+  log('init', `Server listening on port ${config.port}, Ctrl+C to stop`)
 })
