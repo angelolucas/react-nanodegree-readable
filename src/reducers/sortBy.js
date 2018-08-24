dc5394bdca5c573f8fe5f149020fd96a7f8cfb00
Ângelo Lucas <angelo.lrc@gmail.com>
2018-08-24 20:32:24 -0300

    `sortBy` on `localStorage`

--- src/reducers/sortBy.js
+++ src/reducers/sortBy.js
@@ -1,10 +1,11 @@
 import { SORT_BY } from '../actions/sortBy'
 
-const defaultState = 'date'
+const defaultState = localStorage.sort ? localStorage.sort : 'date'
 
 export default function reducer(state = defaultState, action) {
   switch (action.type) {
     case SORT_BY:
+      localStorage.sort = action.option
       return action.option
 
     default:
