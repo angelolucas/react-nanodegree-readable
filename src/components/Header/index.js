0029b0e7f1dec9c42552a835e31e49a25726610b
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-03 15:42:07 -0300

    Remove Navigation component from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite/no-important'
 import { spaces, colors, breakpoint } from '../../theme'
 import Logo from './Logo'
-import Navigation from './Navigation'
+import Categories from './Categories'
+import Menu from './Menu'
 
 class Header extends Component {
   render() {
     return (
       <header className={css(styles.header)}>
         <Logo />
-        <Navigation />
+        <div className={css(styles.navigation)}>
+          <Categories />
+          <Menu />
+        </div>
       </header>
     )
   }
@@ -23,6 +27,13 @@ const styles = StyleSheet.create({
     borderBottom: `1px solid ${colors.border}`,
 
     [breakpoint.medium]: { flexDirection: 'column' },
+  },
+
+  navigation: {
+    display: 'flex',
+    flex: 'auto',
+    alignItems: 'flex-end',
+    position: 'relative',
   },
 })
 
