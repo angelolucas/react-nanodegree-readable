ffcd855b9ab57d53f3c60c0b5449653bd7f4de86
Ângelo Lucas <angelo.lrc@gmail.com>
2018-09-02 22:41:47 -0300

    Remove slugify

--- src/utils/slugify.js
+++
@@ -1,7 +0,0 @@
-import slugify from 'slugify'
-
-export default text =>
-  slugify(text, {
-    remove: /[*+~.()'"!:@—]/g, // regex to remove characters
-    lower: true, // result in lower case
-  })
